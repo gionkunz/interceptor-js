e197f1db4e5986dfb6bceaf9639bac2b737ee2b9
Gion Kunz <gion.kunz@ctp.com>
2014-08-26 12:28:01 +0200

    Added license and banner

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -2,23 +2,41 @@
 
 var gulp = require('gulp');
 var concat = require('gulp-concat');
+var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
+var header = require('gulp-header');
 var sourcemaps = require('gulp-sourcemaps');
 var exorcist = require('exorcist');
 var transform = require('vinyl-transform');
 var wrap = require('gulp-wrap-umd');
 var mkdirp = require('mkdirp');
 
+var banner = ['/**',
+  ' * <%= pkg.name %> - <%= pkg.description %>',
+  ' * @author Gion Kunz',
+  ' * @version v<%= pkg.version %>',
+  ' * @link <%= pkg.homepage %>',
+  ' * Free to use under the WTFPL license.',
+  ' * http://www.wtfpl.net/',
+  ' */',
+  ''].join('\n');
+
+var pkg = require('../../package.json');
+
 gulp.task('scripts', function() {
   // Minify and copy all JavaScript (except vendor scripts)
   // with sourcemaps all the way down
   return gulp.src('./src/**/*.js')
-    .pipe(concat('interceptor.min.js'))
+    .pipe(concat('interceptor.js'))
     .pipe(wrap({
       namespace: 'Interceptor'
     }))
+    .pipe(header(banner, pkg))
+    .pipe(gulp.dest('./dist'))
     .pipe(sourcemaps.init())
     .pipe(uglify())
+    .pipe(header(banner, pkg))
+    .pipe(rename('interceptor.min.js'))
     .pipe(sourcemaps.write())
     // Exorcist requires a vinyl transform to work with gulp
     .pipe(transform(function() {
